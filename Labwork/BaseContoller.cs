d0c40df9d4825e4125b88a93a28655a7d9aa727a
EshDel <ilya-neyzer@mail.ru>
2022-04-05 18:01:47 +0700

    Ada

--- Labwork/BaseContoller.cs
+++ Labwork/BaseContoller.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Labwork
 {
+    //Base Controller
     interface BaseContoller
     {
+        string SelectFile(); // Return path to file
+        void ClearInterface(); // Clear user interface
+        void EditFile(string path);
+        void Synchronization(string path); // sync data from file with user interface
+        List<Item> DataFromFileToListItem(string path);
+        void ErrorMessage(string error);
     }
 }
